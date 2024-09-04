d08803723336a76f4e0f518ebc25461a25e62ed5
akayi <zhuolimiaotx@163.com>
2024-09-04 16:57:24 +0800

    replace rand with urandom to fix module is not an object or function error

--- src/reward.rs
+++ src/reward.rs
@@ -1,42 +1,35 @@
-use rand::Rng;
 use crate::player::{PuppyPlayer, Owner};
 
 #[derive(Debug, Clone)]
 pub enum DrinkReward {
     Champagne,
+    Cognac,
     Whiskey,
     Wine,
-    Cocktail,
-    Beer,
-    Vodka,
-    Rum,
-    Gin,
     Tequila,
-    Cognac,
-    Absinthe
+    Vodka,
+    Cocktail,
+    Beer
 }
 
 impl DrinkReward {
-  pub fn reward_value(&self) -> u64 {
-      match self {
-          DrinkReward::Champagne => 100,
-          DrinkReward::Cognac => 80,
-          DrinkReward::Whiskey => 60,
-          DrinkReward::Wine => 50,
-          DrinkReward::Tequila => 40,
-          DrinkReward::Vodka => 30,
-          DrinkReward::Rum => 20,
-          DrinkReward::Gin => 10,
-          DrinkReward::Cocktail => 70,
-          DrinkReward::Beer => 110,
-          DrinkReward::Absinthe => 90
-      }
-  }
+    pub fn reward_value(&self) -> u64 {
+        match self {
+            DrinkReward::Champagne => 100,
+            DrinkReward::Cognac => 80,
+            DrinkReward::Whiskey => 60,
+            DrinkReward::Wine => 50,
+            DrinkReward::Tequila => 40,
+            DrinkReward::Vodka => 30,
+            DrinkReward::Cocktail => 70,
+            DrinkReward::Beer => 10
+        }
+    }
 }
 
 fn select_random_drink_reward() -> DrinkReward {
-    let mut rng = rand::thread_rng();
-    let reward = match rng.gen_range(0..=100) {
+    let mut rng = urandom::new();
+    let reward = match rng.range(0..=100) {
         0..=10 => DrinkReward::Champagne,   // 10% chance
         11..=20 => DrinkReward::Cognac,     // 10% chance
         21..=40 => DrinkReward::Whiskey,    // 20% chance
@@ -46,12 +39,13 @@ fn select_random_drink_reward() -> DrinkReward {
         81..=100 => DrinkReward::Beer,      // 20% chance
         _ => DrinkReward::Cocktail,         // Default to Cocktail (unlikely)
     };
+
     reward
 }
 
 pub fn assign_reward_to_player(player: &mut PuppyPlayer) {
-  let reward = select_random_drink_reward();
-  player.set_reward(reward.reward_value());
-  let player_id = player.player_id;
-  zkwasm_rust_sdk::dbg!("Player {:?} has received a reward: {:?}", player_id, reward);
+    let reward = select_random_drink_reward();
+    player.set_reward(reward.reward_value());
+    let player_id = player.player_id;
+    zkwasm_rust_sdk::dbg!("Player {:?} has received a reward", player_id);
 }
