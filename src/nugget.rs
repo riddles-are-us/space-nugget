4f1ffb71f9d565db2b9a72e240d8b9699bbdb97d
xgao <xgao@zoyoe.com>
2025-05-13 20:16:42 +1000

    fix numeric

--- src/nugget.rs
+++ src/nugget.rs
@@ -42,7 +42,7 @@ impl StorageData for NuggetInfo {
 }
 
 const EXPLORE_WEIGHT:[u8; 64] = [
-    2,2,2,2,2,2,1,0,
+    2,2,2,1,1,1,1,0,
     3,3,3,3,3,2,1,0,
     4,4,4,4,3,2,1,0,
     5,5,5,4,3,2,1,0,
@@ -58,7 +58,7 @@ impl NuggetInfo {
        NuggetInfo {
            id,
            cycle: 0,
-           attributes: [c[0].bitxor(c[1]) + 1, 0, 0, 0, 0, 0, 0, 0],
+           attributes: [c[0].bitxor(c[1]) / 2 + 1, 0, 0, 0, 0, 0, 0, 0],
            feature: rand % 8,
            sysprice: 0,
            marketid: 0,
