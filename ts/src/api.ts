4116183126ed8f2c7d0474bc1f00d8cc2c71500f
xgao <xgao@zoyoe.com>
2025-05-18 20:22:59 +1000

    fix tests

--- ts/src/api.ts
+++ ts/src/api.ts
@@ -1,13 +1,50 @@
 import { ZKWasmAppRpc, PlayerConvention, createCommand } from "zkwasm-minirollup-rpc";
 import BN from "bn.js";
 
-
-
 // for withdraw
 const address = "c177d1d314C8FFe1Ea93Ca1e147ea3BE0ee3E470";
 const amount = 1n;
 
 const rpc = new ZKWasmAppRpc("http://localhost:3000");
+
+export const commandSpec = {
+  INSTALL_PLAYER: {
+      id: 1n,
+      args: []
+  },
+  WITHDRAW: {
+      id: 2n,
+      args: []
+  },
+  DEPOSIT: {
+      id: 3n,
+      args: []
+  },
+  EXPLORE_NUGGET: {
+      id: 4n,
+      args: ["index"]
+  },
+  SELL_NUGGET: {
+      id: 5n,
+      args: ["marketid"]
+  },
+  BID_NUGGET: {
+      id: 6n,
+      args: ["marketid", "price"]
+  },
+  CREATE_NUGGET: {
+      id: 7n,
+      args: []
+  },
+  RECYCLE_NUGGET: {
+      id: 8n,
+      args: ["index"]
+  },
+  LIST_NUGGET: {
+      id: 9n,
+      args: ["index", "price"]
+  }
+}
 
 export class Player extends PlayerConvention {
   constructor(key: string, rpc: ZKWasmAppRpc, deposit: bigint, withdraw: bigint) {
--- ts/src/test.ts
+++ ts/src/test.ts
@@ -1,22 +1,12 @@
-import { Player } from "./api.js";
+import { Player, commandSpec } from "./api.js";
 //import { LeHexBN, ZKWasmAppRpc} from "zkwasm-minirollup-rpc";
 import { LeHexBN, query, ZKWasmAppRpc} from "zkwasm-ts-server";
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const INSTALL_PLAYER = 1n;
-const EXPLORE_NUGGET = 4n;
-const SELL_NUGGET = 5n;
-const BID_NUGGET = 6n;
-const CREATE_NUGGET = 7n;
-const WITHDRAW = 8n;
-const DEPOSIT = 9n;
-const RECYCLE_NUGGET = 10n;
-const LIST_NUGGET = 11n;
-
 let account = "1234";
 
 const rpc:any = new ZKWasmAppRpc("http://127.0.0.1:3000");
-let player = new Player(account, rpc, DEPOSIT, WITHDRAW);
+let player = new Player(account, rpc, commandSpec.DEPOSIT.id, commandSpec.WITHDRAW.id);
 
 // Function to pause execution for a given duration
 function delay(ms: number) {
@@ -31,7 +21,7 @@ async function main() {
   console.log("config:", r);
 
   console.log("Start run CREATE_PLAYER...");
-  await player.runCommand(INSTALL_PLAYER, 0n, []);
+  await player.runCommand(commandSpec.INSTALL_PLAYER.id, 0n, []);
 
   let g = await player.getState();
   console.log("state.", g);
@@ -39,15 +29,15 @@ async function main() {
 
   console.log("Start run CREATE_NUGGET ...");
   let nonce = await player.getNonce();
-  await player.runCommand(CREATE_NUGGET, nonce, []);
+  await player.runCommand(commandSpec.CREATE_NUGGET.id, nonce, []);
 
   console.log("Start run EXPLORE_NUGGET ...");
   nonce = await player.getNonce();
-  await player.runCommand(EXPLORE_NUGGET, nonce, [0n]);
+  await player.runCommand(commandSpec.EXPLORE_NUGGET.id, nonce, [0n]);
 
   console.log("Start run LIST_NUGGET ...");
   nonce = await player.getNonce();
-  await player.runCommand(LIST_NUGGET, nonce, [0n, 10n]);
+  await player.runCommand(commandSpec.LIST_NUGGET.id, nonce, [0n, 10n]);
 
   console.log("Start run query nuggets ...");
   try {
@@ -75,7 +65,7 @@ async function main() {
     console.log("Start run BID_NUGGET ...");
     console.log(JSON.stringify(data.data[0]));
     nonce = await player.getNonce();
-    await player.runCommand(BID_NUGGET, nonce, [BigInt(marketid), 8n]);
+    await player.runCommand(commandSpec.BID_NUGGET.id, nonce, [BigInt(marketid), 8n]);
   } catch(e) {
     console.log(e);
   }
@@ -91,7 +81,7 @@ async function main() {
   try {
     console.log("SELL NUGGET ...");
     nonce = await player.getNonce();
-    await player.runCommand(SELL_NUGGET, nonce, [BigInt(marketid)]);
+    await player.runCommand(commandSpec.SELL_NUGGET.id, nonce, [BigInt(marketid)]);
   } catch(e) {
     console.log(e);
   }
