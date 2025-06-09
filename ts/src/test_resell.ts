2204d4c738cba1e802694203550c4358f2ef75ca
charlie275691502 <charlie275691502@gmail.com>
2025-06-09 17:56:01 +1000

    Add resell testing code

---
+++ ts/src/test_resell.ts
@@ -0,0 +1,79 @@
+import { Player, commandSpec } from "./api.js";
+//import { LeHexBN, ZKWasmAppRpc} from "zkwasm-minirollup-rpc";
+import { LeHexBN, query, ZKWasmAppRpc } from "zkwasm-ts-server";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+let account = "1234";
+let account_bid = "5678";
+
+const rpc: any = new ZKWasmAppRpc("http://127.0.0.1:3000");
+let player = new Player(
+  account,
+  rpc,
+  commandSpec.DEPOSIT.id,
+  commandSpec.WITHDRAW.id
+);
+let player_bid = new Player(
+  account_bid,
+  rpc,
+  commandSpec.DEPOSIT.id,
+  commandSpec.WITHDRAW.id
+);
+
+// Function to pause execution for a given duration
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  const pubkey = new LeHexBN(query(account).pkx).toU64Array();
+  console.log(pubkey);
+
+  let r = await player.rpc.queryConfig();
+  console.log("config:", r);
+
+  console.log("Start run CREATE_PLAYER...");
+  await player.runCommand(commandSpec.INSTALL_PLAYER.id, 0n, []);
+  await player_bid.runCommand(commandSpec.INSTALL_PLAYER.id, 0n, []);
+
+  console.log("Start run CREATE_NUGGET ...");
+  let nonce = await player.getNonce();
+  await player.runCommand(commandSpec.CREATE_NUGGET.id, nonce, []);
+
+  console.log("Start run LIST_NUGGET ...");
+  nonce = await player.getNonce();
+  await player.runCommand(commandSpec.LIST_NUGGET.id, nonce, [0n, 10n]);
+
+  console.log("Start run query auction...");
+  try {
+    let data: any = await player.rpc.queryData(`markets`);
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    console.log("Start run BID_NUGGET ...");
+    let nonce_bid = await player_bid.getNonce();
+    await player_bid.runCommand(commandSpec.BID_NUGGET.id, nonce_bid, [
+      BigInt("1"),
+      10n,
+    ]);
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log("Start run LIST_NUGGET ...");
+  let nonce_bid = await player_bid.getNonce();
+  await player_bid.runCommand(commandSpec.LIST_NUGGET.id, nonce_bid, [0n, 15n]);
+
+  console.log("Start run query auction...");
+  try {
+    let data: any = await player.rpc.queryData(`markets`);
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+main();
